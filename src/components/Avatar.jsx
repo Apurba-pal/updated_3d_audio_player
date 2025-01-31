f7fc8054526874dd432cf6fad30957f2615aebff
Apurba <palapurba2004@gmail.com>
2025-01-31 21:39:07 +0530

    removed leva created automatic

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -7,9 +7,7 @@ npx gltfjsx public/models/debarun.glb -o src/components/Avatar2.jsx -r public
 
 import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
 import { useFrame, useLoader } from "@react-three/fiber";
-import { useControls } from "leva";
 import React, { useEffect, useMemo, useRef, useState } from "react";
-
 import * as THREE from "three";
 
 const corresponding = {
@@ -26,162 +24,24 @@ const corresponding = {
 };
 
 export function Avatar({ audioUrl, lipsyncData, ...props }) {
-  const {
-    playAudio,
-    script,
-    headFollow,
-    smoothMorphTarget,
-    morphTargetSmoothing,
-  } = useControls({
-    playAudio: false,
-    headFollow: false,
-    smoothMorphTarget: true,
-    morphTargetSmoothing: 0.5,
-    script: {
-      value: "welcome",
-      // options: ["welcome", "pizzas"],
-      options: ["welcome", "pizzas_j", "sample_audio"],
-    },
-  });
-
-  const audio = useMemo(() => new Audio(`/audios/${script}.mp3`), [script]);
-  const jsonFile = useLoader(THREE.FileLoader, `audios/${script}.json`);
-  const lipsync = JSON.parse(jsonFile);
-
-  useFrame(() => {
-    const currentAudioTime = audio.currentTime;
-
-    // If audio is paused or ended, reset to Idle animation
-    if (audio.paused || audio.ended) {
-      setAnimation("Idle");
-      return;
-    }
-
-    // Reset all morph targets for lipsync to 0
-    Object.values(corresponding).forEach((value) => {
-      if (nodes.Wolf3D_Head && nodes.Wolf3D_Teeth) {
-        const headTarget = nodes.Wolf3D_Head.morphTargetDictionary[value];
-        const teethTarget = nodes.Wolf3D_Teeth.morphTargetDictionary[value];
-
-        if (smoothMorphTarget) {
-          // Smoothly reset morph targets
-          nodes.Wolf3D_Head.morphTargetInfluences[headTarget] =
-            THREE.MathUtils.lerp(
-              nodes.Wolf3D_Head.morphTargetInfluences[headTarget],
-              0,
-              morphTargetSmoothing
-            );
-          nodes.Wolf3D_Teeth.morphTargetInfluences[teethTarget] =
-            THREE.MathUtils.lerp(
-              nodes.Wolf3D_Teeth.morphTargetInfluences[teethTarget],
-              0,
-              morphTargetSmoothing
-            );
-        } else {
-          // Instantly reset morph targets
-          nodes.Wolf3D_Head.morphTargetInfluences[headTarget] = 0;
-          nodes.Wolf3D_Teeth.morphTargetInfluences[teethTarget] = 0;
-        }
-      }
-    });
-
-    // Apply active viseme based on audio's current time and mouthCues
-    for (let i = 0; i < lipsync.mouthCues.length; i++) {
-      const mouthCue = lipsync.mouthCues[i];
-
-      if (
-        currentAudioTime >= mouthCue.start &&
-        currentAudioTime <= mouthCue.end
-      ) {
-        const visemeName = corresponding[mouthCue.value];
-
-        if (nodes.Wolf3D_Head && nodes.Wolf3D_Teeth) {
-          const headTarget =
-            nodes.Wolf3D_Head.morphTargetDictionary[visemeName];
-          const teethTarget =
-            nodes.Wolf3D_Teeth.morphTargetDictionary[visemeName];
-
-          if (smoothMorphTarget) {
-            // Smoothly apply active morph target
-            nodes.Wolf3D_Head.morphTargetInfluences[headTarget] =
-              THREE.MathUtils.lerp(
-                nodes.Wolf3D_Head.morphTargetInfluences[headTarget],
-                1,
-                morphTargetSmoothing
-              );
-            nodes.Wolf3D_Teeth.morphTargetInfluences[teethTarget] =
-              THREE.MathUtils.lerp(
-                nodes.Wolf3D_Teeth.morphTargetInfluences[teethTarget],
-                1,
-                morphTargetSmoothing
-              );
-          } else {
-            // Instantly apply active morph target
-            nodes.Wolf3D_Head.morphTargetInfluences[headTarget] = 1;
-            nodes.Wolf3D_Teeth.morphTargetInfluences[teethTarget] = 1;
-          }
-        }
-        break; // Stop processing further cues for this frame
-      }
-    }
-  });
-
-  useEffect(() => {
-    // console.log(nodes.Wolf3D_Head.morphTargetDictionary)
-    nodes.Wolf3D_Head.morphTargetInfluences[
-      nodes.Wolf3D_Head.morphTargetDictionary["viseme_I"]
-    ] = 1;
-    nodes.Wolf3D_Teeth.morphTargetInfluences[
-      nodes.Wolf3D_Teeth.morphTargetDictionary["viseme_I"]
-    ] = 1;
-    if (playAudio) {
-      audio.play();
-      if (script === "welcome") {
-        setAnimation("Greeting");
-      } else {
-        setAnimation("Angry");
-      }
-    } else {
-      setAnimation("Idle");
-      audio.pause();
-    }
-  }, [playAudio, script]);
-
-  // const { nodes, materials } = useGLTF("/models/646d9dcdc8a5f5bddbfac913.glb");
+  const [animation, setAnimation] = useState("Idle");
+  const group = useRef();
   const { nodes, materials } = useGLTF("/models/debarun.glb");
   const { animations: idleAnimation } = useFBX("/animations/Idle.fbx");
-  const { animations: angryAnimation } = useFBX(
-    "/animations/Angry Gesture.fbx"
-  );
-  const { animations: greetingAnimation } = useFBX(
-    "/animations/Standing Greeting.fbx"
-  );
-  // console.log(nodes);
-  // console.log(materials);
+  const { animations: angryAnimation } = useFBX("/animations/Angry Gesture.fbx");
+  const { animations: greetingAnimation } = useFBX("/animations/Standing Greeting.fbx");
+  const { actions } = useAnimations([idleAnimation[0], angryAnimation[0], greetingAnimation[0]], group);
 
   idleAnimation[0].name = "Idle";
   angryAnimation[0].name = "Angry";
   greetingAnimation[0].name = "Greeting";
 
-  const [animation, setAnimation] = useState("Idle");
-
-  const group = useRef();
-  const { actions } = useAnimations(
-    [idleAnimation[0], angryAnimation[0], greetingAnimation[0]],
-    group
-  );
-
   useEffect(() => {
-    actions[animation].reset().fadeIn(0.5).play();
-    return () => actions[animation].fadeOut(0.5);
-  }, [animation]);
-
-  // CODE ADDED AFTER THE TUTORIAL (but learnt in the portfolio tutorial ♥️)
-  useFrame((state) => {
-    if (headFollow) {
-      group.current.getObjectByName("Head").lookAt(state.camera.position);
+    if (actions && actions[animation]) {
+      actions[animation].reset().fadeIn(0.5).play();
+      return () => actions[animation].fadeOut(0.5);
     }
-  });
+  }, [animation, actions]);
 
   useEffect(() => {
     if (audioUrl && lipsyncData) {
@@ -206,17 +66,12 @@ export function Avatar({ audioUrl, lipsyncData, ...props }) {
         for (let i = 0; i < lipsyncData.mouthCues.length; i++) {
           const mouthCue = lipsyncData.mouthCues[i];
 
-          if (
-            currentAudioTime >= mouthCue.start &&
-            currentAudioTime <= mouthCue.end
-          ) {
+          if (currentAudioTime >= mouthCue.start && currentAudioTime <= mouthCue.end) {
             const visemeName = corresponding[mouthCue.value];
 
             if (nodes.Wolf3D_Head && nodes.Wolf3D_Teeth) {
-              const headTarget =
-                nodes.Wolf3D_Head.morphTargetDictionary[visemeName];
-              const teethTarget =
-                nodes.Wolf3D_Teeth.morphTargetDictionary[visemeName];
+              const headTarget = nodes.Wolf3D_Head.morphTargetDictionary[visemeName];
+              const teethTarget = nodes.Wolf3D_Teeth.morphTargetDictionary[visemeName];
 
               nodes.Wolf3D_Head.morphTargetInfluences[headTarget] = 1;
               nodes.Wolf3D_Teeth.morphTargetInfluences[teethTarget] = 1;
@@ -339,6 +194,3 @@ export function Avatar({ audioUrl, lipsyncData, ...props }) {
 }
 
 useGLTF.preload("/models/debarun.glb");
-// useGLTF.preload("/models/646d9dcdc8a5f5bddbfac913.glb");
-
-// ?morphTargets=Oculus Visemes
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,36 +12,14 @@ export const Experience = ({ audioUrl, lipsyncData }) => {
       <Avatar position={[0, -1.5, 0]} scale={1} audioUrl={audioUrl} lipsyncData={lipsyncData} />
       <Environment preset="sunset" />
       <mesh>
-        {/* <planeGeometry args={[viewport.width, viewport.height]} /> */}
-        {/* <planeGeometry args={[viewport.width, viewport.height]} rotateX={90} /> */}
-        {/* <meshBasicMaterial map={texture} /> */}
-        {/* <meshBasicMaterial color="white" /> */}
       </mesh>
       <mesh position={[0, 0, -40]}>
         <boxGeometry args={[100, 100, 0.1]} />
         <meshBasicMaterial color="white" map={texture} />
       </mesh>
       <mesh position={[0, 0, 5]}>
-        {/* <boxGeometry args={[1, 1, 1]} /> */}
-        {/* <planeGeometry args={[10,10]}/> */}
         <meshBasicMaterial color="white" transparent opacity={0} />
       </mesh>
-      {/* <mesh position={[-5, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
-        <boxGeometry args={[10, 10, 0.1]} />
-        <meshBasicMaterial color="black" />
-      </mesh> */}
-      {/* <mesh position={[5, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
-        <boxGeometry args={[10, 10, 0.1]} />
-        <meshBasicMaterial color="black" />
-      </mesh> */}
-      {/* <mesh position={[0, 5, 0]} rotation={[Math.PI / 2, 0, 0]}>
-        <boxGeometry args={[10, 10, 0.1]} />
-        <meshBasicMaterial color="black" />
-      </mesh> */}
-      {/* <mesh position={[0, -5, 0]} rotation={[Math.PI / 2, 0, 0]}>
-        <boxGeometry args={[10, 10, 0.1]} />
-        <meshBasicMaterial color="black" />
-      </mesh> */}
     </>
   );
 };
